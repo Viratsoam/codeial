6ff80e9fcb079bf77c584bf58301ffb07fbe76d0
Viratsoam <viratsoam1105>
2020-04-29 14:42:00 +0530

    adeed the controller and route for home

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 const port = process.env.PORT | 8000;
 
 
+// use express routes
+
+app.use('/',require('./routes'));
+
+
+
 app.listen(port,(err)=>{
     if(err){
         console.log(`Server is not responding:${err}`);
@@ -10,4 +16,4 @@ app.listen(port,(err)=>{
     }
     console.log(`Server is up on port:${port}`);
 
-})
+});
