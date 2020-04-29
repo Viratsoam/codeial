a6072f80458e764a15e9e7ce2f7ae0507df5f703
Viratsoam <viratsoam1105>
2020-04-29 12:48:54 +0530

    fire the express server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT | 8000;
+
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(`Server is not responding:${err}`);
+        return;
+    }
+    console.log(`Server is up on port:${port}`);
+
+})
