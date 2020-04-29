2a27ced28d8206983360fdeb9c75f87033c24a7e
Viratsoam <viratsoam1105>
2020-04-29 18:36:35 +0530

    added the static files structure

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const port = process.env.PORT | 8000;
 const expressLayouts = require('express-ejs-layouts');
 
 // set up the static file
-
 app.use(express.static('./assests'))
 
 // use express-ejs-layouts
 app.use(expressLayouts);
 
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts',true);
 // use express routes
 app.use('/',require('./routes'));
 
